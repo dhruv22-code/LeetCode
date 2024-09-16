89ada5d201e2931351fd0f6fec09bb6ea1a96ff5
Dhruba laskar <69336624+dhruv22-code@users.noreply.github.com>
2024-09-16 23:39:20 +0530

    Time: 9 ms (82.5%), Space: 17.3 MB (98.24%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
         int mini = INT_MAX;
-        for(int i = 0 ; i < timePoints.size(); i++){
-            for(int j = i + 1; j < timePoints.size() ; j++){
-                if(timePoints[i][2] == ':')
-                timePoints[i].erase(2, 1);
-                if(timePoints[j][2] == ':')
-                timePoints[j].erase(2, 1);
-                int a = ((timePoints[i][0] - '0') * 10 + timePoints[i][1] - '0')* 60 + ((timePoints[i][2] - '0') * 10 + timePoints[i][3] - '0');
-                int b = ((timePoints[j][0] - '0') * 10 + timePoints[j][1] - '0')* 60 + ((timePoints[j][2] - '0') * 10 + timePoints[j][3] - '0');
+        vector<int> v(timePoints.size(), 0);
+        for (int i = 0; i < timePoints.size(); i++) {
 
-                int c= min(abs(a - b), abs(1440 - abs(a- b)));
-                
-                mini = min(mini, c);
-            }
+            timePoints[i].erase(2, 1);
+            v[i] =
+                ((timePoints[i][0] - '0') * 10 + timePoints[i][1] - '0') * 60 +
+                ((timePoints[i][2] - '0') * 10 + timePoints[i][3] - '0');
         }
-        return mini;
+        sort(v.begin(), v.end());
+        for (int i = 0; i < timePoints.size() - 1; i++) {
+            int c = min(abs(v[i] - v[i + 1]), abs(1440 - abs(v[i] - v[i + 1])));
+            mini = min(mini, c);
+        }
+
+        mini = min(mini, abs(v[timePoints.size() - 1] - v[0]));
+        return min(mini, abs(1440 - abs(v[timePoints.size() - 1] - v[0])));
     }
 };
