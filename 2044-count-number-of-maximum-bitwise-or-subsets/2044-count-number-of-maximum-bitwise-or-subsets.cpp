5639134bf5defa7128752d9ab7936528405815cb
Dhruba laskar <69336624+dhruv22-code@users.noreply.github.com>
2025-07-29 18:41:38 +0530

    Time: 296 ms (13.81%), Space: 144 MB (17.12%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
 
     int solve(vector<int>nums, int index, int curr, int maxi){
-         if (index == nums.size()) {
-            return curr == maxi ? 1 : 0;
-        }
+         if(curr == maxi)return pow(2, nums.size() - index);
+         if(index == nums.size())return 0;
         int skip = solve(nums, index + 1, curr, maxi);
         int take = solve(nums, index + 1, curr | nums[index], maxi);
         return skip+take;
