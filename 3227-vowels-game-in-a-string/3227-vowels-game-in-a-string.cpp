56a5c6db1b394309296de71f49d224ab514768f3
Dhruba laskar <69336624+dhruv22-code@users.noreply.github.com>
2025-09-12 23:00:49 +0530

    Time: 4 ms (78.54%), Space: 19 MB (53.73%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -3,9 +3,9 @@ public:
     bool doesAliceWin(string s) {
         int count = 0;
      for(int i = 0 ; i < s.length() ; i++){
-        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' )count++;
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' )return true;
      }   
-     if(count == 0 )return false;
-     else return true;
+    return false;
+   
     }
 };
