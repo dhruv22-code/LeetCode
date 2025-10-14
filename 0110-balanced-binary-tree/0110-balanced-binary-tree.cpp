5dee5760f8047f0520f5e34b55742e0672dd7b29
Dhruba laskar <69336624+dhruv22-code@users.noreply.github.com>
2025-10-14 16:32:19 +0530

    Time: 0 ms (100%), Space: 23 MB (83.81%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -6,27 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
         if(root == NULL)return true;
-        if(abs(maxDepth(root->left) - maxDepth(root->right)) <= 1) return isBalanced(root->left) && isBalanced(root->right);
-        else return false;
-       
-
-
+        if (abs(maxDepth(root->left) - maxDepth(root->right)) <= 1){
+            return isBalanced(root->left) && isBalanced(root->right);
+        }
+        else
+            return false;
     }
-
-int maxDepth(struct TreeNode* root) {
-    if(root == NULL){
-        return 0;
+     int maxDepth(TreeNode* root) {
+        if(root == NULL)return 0;
+        return max(maxDepth(root->left), maxDepth(root->right))+ 1;
     }
-    int left = maxDepth(root->left);
-    int right =maxDepth(root->right);
-    int ans = (left>right)?left+1:right+1;
-    return ans;
-}
 };
