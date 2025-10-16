5544821227e6af746f3c625c008481b4100b77c7
Dhruba laskar <69336624+dhruv22-code@users.noreply.github.com>
2025-10-16 16:34:17 +0530

    Time: 69 ms (49.08%), Space: 135.6 MB (51.33%) - LeetHub

--- 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.cpp
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.cpp
@@ -3,32 +3,23 @@ public:
     int findSmallestInteger(vector<int>& nums, int value) {
         unordered_map<int, int>f;
         int mini = INT_MAX;
-        for(int i = 0 ; i < nums.size() ; i++){
-            f[nums[i] % value]++;
-            
+         for (int x : nums) {
+            int rem = ((x % value) + value) % value;
+            f[rem]++;
         }
-       
         for(int i = 0 ; i < value ; i++){
             if(f.find(i) == f.end()){
                 return i;
             }
+            mini = min(mini, f[i]);
         }
-         for(int i = 0 ; i < nums.size() ; i++){
-            mini = min(mini, f[nums[i] % value]);
-            
+      
+                  for (int i = 0; i < value; i++) {
+            if (f[i] == mini)
+                return i + mini * value;
         }
-        
-        if(f[0] == mini){
-            return value * mini;
-        }else{
-            int i = 0, ans = 0;
-            
-            while( i < value && f[i] > mini){
-                ans = i + value*(mini);
-                i++;
-            }
-            return ans;
-        }
-        return 0;
+            return 0;
+       
+     
     }
 }; 
