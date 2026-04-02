a4c9adee1b5aa4ffaede0be2ae81efaeb52423b6
Dhruba laskar <69336624+dhruv22-code@users.noreply.github.com>
2026-04-03 01:56:09 +0530

    Time: 0 ms (100%), Space: 8.9 MB (30.75%) - LeetHub

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+        for(int i = 0 ; i <= s.length()/2 ; i++){
+            if(s[i] == s[s.length() - i - 1])return i;
+        }
+        return -1;
+    }
+};
