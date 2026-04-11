abf76bb1716a0a65a5d1897fea118b6f6c4efd1d
Dhruba laskar <69336624+dhruv22-code@users.noreply.github.com>
2026-04-11 19:32:09 +0530

    Time: 392 ms (5.01%), Space: 58.8 MB (57.98%) - LeetHub

---
+++ 3889-mirror-frequency-distance/3889-mirror-frequency-distance.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mirrorFrequency(string s) {
+        unordered_map<char, int>f;
+        for(int i = 0 ; i < s.length(); i++){
+            f[s[i]]++;
+        }
+        int ans = 0;
+        for(int i = 0 ; i < s.length(); i++){
+            if(f[s[i]] > 0 && s[i] >= 48 && s[i] <= 57){
+                ans += abs(f[s[i]] - f[57 - (s[i] - 48)]);
+                f[57 - (s[i] - 48)] = 0;
+            }else if(f[s[i]] > 0){
+                ans += abs(f[s[i]] - f[122 - (s[i] - 97)]);
+                f[122 - (s[i] - 97)] = 0;
+            }
+            f[s[i]] = 0;
+
+        }
+        return ans;
+
+    }
+};
