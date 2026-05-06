e6cb0aa0e7e1be79084827e6b822a1b4db19ec92
Dhruba laskar <69336624+dhruv22-code@users.noreply.github.com>
2026-05-06 11:09:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.cpp
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
-        int count = 0;
-        vector<vector<char>>v(boxGrid.size() , vector<char>(boxGrid[0].size(), '.'));
-        vector<vector<char>>v1(boxGrid[0].size() , vector<char>(boxGrid.size(), '.'));
-        for(int i = 0 ; i < boxGrid.size() ; i++){
-            for(int j = 0 ; j < boxGrid[0].size(); j++){
-                if(boxGrid[i][j] == '*' ){
-                    v[i][j] = '*';
-                    while(count){
-                        v[i][j - count] = '#';
-                        count--;
-                    }
-                }else if(boxGrid[i][j] == '#')count++;
-                 if( j == boxGrid[0].size() - 1){
-                    int k = j;
-                    while(v[i][k] == '#'){k--;count--;}
-                     
-                    while(count){
-                        v[i][k - count + 1] = '#';
-                        count--;
-                    }
+        int m = boxGrid.size();
+        int n = boxGrid[0].size();
+
+        // Step 1: Apply gravity
+        for(int i = 0; i < m; i++) {
+            int empty = n - 1;
+
+            for(int j = n - 1; j >= 0; j--) {
+                if(boxGrid[i][j] == '*') {
+                    empty = j - 1;
                 }
-                    
+                else if(boxGrid[i][j] == '#') {
+                    swap(boxGrid[i][j], boxGrid[i][empty]);
+                    empty--;
+                }
             }
         }
-        
-            for(int j = 0 ; j < boxGrid[0].size(); j++){
-                for(int i = boxGrid.size() - 1; i >= 0 ; i--){
-                   v1[j][boxGrid.size() - i - 1] =  v[i][j] ;
-                }
+
+        // Step 2: Rotate 90 degrees clockwise
+        vector<vector<char>> res(n, vector<char>(m));
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                res[j][m - i - 1] = boxGrid[i][j];
             }
-            return v1;
-        
+        }
+
+        return res;
     }
 };
